cc14c0b2d5d0aeb434632dbccb3d6e244f852ffb
lioliu <744596028@qq.com>
2017-07-12 21:51:59 +0800

    Add Sign form and Customer class
    add Sign form for the customer user to sign up.
    by the way add the customer class file.

---
+++ WMS/WMS/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WMS
+{
+    class Customer
+    {
+
+
+        public Boolean SignUp()
+        {
+            return false;
+        }
+    }
+}
---
+++ WMS/WMS/Sign.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WMS
+{
+    public partial class Sign : Form
+    {
+        public Sign()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (var item in this.Controls)
+            {
+                if (item is TextBox)
+                {
+                    ((TextBox)item).Text = string.Empty;
+                }
+            }
+        }
+    }
+}
