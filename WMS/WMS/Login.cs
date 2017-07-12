efef97a81712a4f9ff59cffe9db6e3a2f06a99f1
lioliu <744596028@qq.com>
2017-07-12 20:38:42 +0800

    Add Login
    Add Login Form to login the system .
    Two type of user : staff & customer.

---
+++ WMS/WMS/Login.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WMS
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            cbxType.SelectedIndex = 0;
+        }
+
+        private void cbxType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Reset();
+
+            if (cbxType.SelectedIndex == 0)
+            {
+                lSign.Visible = false;
+                lID.Text = "ID:";
+            }
+            else
+            {
+                lSign.Visible = !false;
+                lID.Text = "邮箱:";
+            }
+        }
+
+        private void bReset_Click(object sender, EventArgs e)
+        {
+            Reset();
+
+        }
+
+
+
+        private void Reset()
+        {
+            textBox1.Text = string.Empty;
+            textBox2.Text = string.Empty;
+        }
+
+        private void bLogin_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
