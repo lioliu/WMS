8db3c98d8ac5d1e94204c88daea391f842613b6f
lioliu <744596028@qq.com>
2017-07-13 11:30:02 +0800

    Add product setting Form

---
+++ WMS/WMS/Product.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace WMS
+{
+    partial class Product
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Product
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(989, 491);
+            this.Name = "Product";
+            this.Text = "货物";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
--- WMS/WMS/StaffMain.Designer.cs
+++ WMS/WMS/StaffMain.Designer.cs
@@ -28,11 +28,138 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.账户管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.密码修改ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.信息修改ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.员工类型修改ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.仓库信息配置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.仓库信息ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.仓储区域信息ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.区域种类信息ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.入库单处理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.出库单处理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.巡检ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.账户管理ToolStripMenuItem,
+            this.仓库信息配置ToolStripMenuItem,
+            this.入库单处理ToolStripMenuItem,
+            this.出库单处理ToolStripMenuItem,
+            this.巡检ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(859, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 账户管理ToolStripMenuItem
+            // 
+            this.账户管理ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.密码修改ToolStripMenuItem,
+            this.信息修改ToolStripMenuItem,
+            this.员工类型修改ToolStripMenuItem});
+            this.账户管理ToolStripMenuItem.Name = "账户管理ToolStripMenuItem";
+            this.账户管理ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
+            this.账户管理ToolStripMenuItem.Text = "账户管理";
+            // 
+            // 密码修改ToolStripMenuItem
+            // 
+            this.密码修改ToolStripMenuItem.Name = "密码修改ToolStripMenuItem";
+            this.密码修改ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.密码修改ToolStripMenuItem.Text = "密码修改";
+            // 
+            // 信息修改ToolStripMenuItem
+            // 
+            this.信息修改ToolStripMenuItem.Name = "信息修改ToolStripMenuItem";
+            this.信息修改ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.信息修改ToolStripMenuItem.Text = "信息修改";
+            // 
+            // 员工类型修改ToolStripMenuItem
+            // 
+            this.员工类型修改ToolStripMenuItem.Name = "员工类型修改ToolStripMenuItem";
+            this.员工类型修改ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.员工类型修改ToolStripMenuItem.Text = "员工类型修改";
+            // 
+            // 仓库信息配置ToolStripMenuItem
+            // 
+            this.仓库信息配置ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.仓库信息ToolStripMenuItem,
+            this.仓储区域信息ToolStripMenuItem,
+            this.区域种类信息ToolStripMenuItem});
+            this.仓库信息配置ToolStripMenuItem.Name = "仓库信息配置ToolStripMenuItem";
+            this.仓库信息配置ToolStripMenuItem.Size = new System.Drawing.Size(92, 21);
+            this.仓库信息配置ToolStripMenuItem.Text = "仓库信息配置";
+            // 
+            // 仓库信息ToolStripMenuItem
+            // 
+            this.仓库信息ToolStripMenuItem.Name = "仓库信息ToolStripMenuItem";
+            this.仓库信息ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.仓库信息ToolStripMenuItem.Text = "仓库信息";
+            // 
+            // 仓储区域信息ToolStripMenuItem
+            // 
+            this.仓储区域信息ToolStripMenuItem.Name = "仓储区域信息ToolStripMenuItem";
+            this.仓储区域信息ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.仓储区域信息ToolStripMenuItem.Text = "仓储区域信息";
+            // 
+            // 区域种类信息ToolStripMenuItem
+            // 
+            this.区域种类信息ToolStripMenuItem.Name = "区域种类信息ToolStripMenuItem";
+            this.区域种类信息ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.区域种类信息ToolStripMenuItem.Text = "区域种类信息";
+            // 
+            // 入库单处理ToolStripMenuItem
+            // 
+            this.入库单处理ToolStripMenuItem.Name = "入库单处理ToolStripMenuItem";
+            this.入库单处理ToolStripMenuItem.Size = new System.Drawing.Size(80, 21);
+            this.入库单处理ToolStripMenuItem.Text = "入库单处理";
+            // 
+            // 出库单处理ToolStripMenuItem
+            // 
+            this.出库单处理ToolStripMenuItem.Name = "出库单处理ToolStripMenuItem";
+            this.出库单处理ToolStripMenuItem.Size = new System.Drawing.Size(80, 21);
+            this.出库单处理ToolStripMenuItem.Text = "出库单处理";
+            // 
+            // 巡检ToolStripMenuItem
+            // 
+            this.巡检ToolStripMenuItem.Name = "巡检ToolStripMenuItem";
+            this.巡检ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.巡检ToolStripMenuItem.Text = "巡检";
+            // 
+            // StaffMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(859, 559);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "StaffMain";
             this.Text = "StaffMain";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 账户管理ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 密码修改ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 信息修改ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 员工类型修改ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 仓库信息配置ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 仓库信息ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 仓储区域信息ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 区域种类信息ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 入库单处理ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 出库单处理ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 巡检ToolStripMenuItem;
     }
 }
